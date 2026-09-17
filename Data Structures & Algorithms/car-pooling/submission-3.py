dba195d23b55d68ac75047f15c58bdf8a0f63ffd
Euikwang (Josh) Kim <66141607+Josh0323@users.noreply.github.com>
2026-09-17 18:11:35 -0400

    Add: car-pooling - submission-3

---
+++ Data Structures & Algorithms/car-pooling/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        sec = []
+        for num, frm, to in trips:
+            sec.append((to, -num))
+            sec.append((frm, num))
+        
+        sec.sort()
+
+        cur_cap = 0
+        for loc, num in sec:
+            cur_cap += num
+            if cur_cap > capacity:
+                return False
+        
+        return True
